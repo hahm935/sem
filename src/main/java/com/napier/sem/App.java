5775ccc18fbb65c7678501c7ce1746b07ff45834
haahm <haahmed935@gmail.com>
2025-09-26 16:24:00 +0100

    java 17 fix

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,11 @@
 package com.napier.sem;
 
 public class App {
-    static void main() {
-            System.out.println("I HATE JAVA");
+    public static void main() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("I HATE JAVA");
     }
 }
